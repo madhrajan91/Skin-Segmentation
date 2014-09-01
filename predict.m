850ce20a380c89500021bbccec0dce616c01cecb
Madhav <madh.rajan91@gmail.com>
2014-09-01 10:17:39 -0700

    Vectorized gradient descent in costFunction.m

--- predict.m
+++ predict.m
@@ -6,14 +6,10 @@ function p = predict(theta, X)
 
 m = size(X, 1); % Number of training examples
 
-% You need to return the following variables correctly
+
 p = zeros(m, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
-%
+
 
 prediction = sigmoid(X*theta);
 for i=1:m
